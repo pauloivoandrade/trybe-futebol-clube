e226aa867a10a49058ea7bed09d06b7ef2612098
pauloivoandrade <pauloivoasantos@gmail.com>
2024-03-08 19:33:01 -0300

    Feat: testes consertados

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,52 +1,38 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
-import chaiHttp from 'chai-http';
+import chaiHttp = require('chai-http');
 
-import { App } from '../app'
-import teamsMock from './mocks/team.mock';
+import { app } from '../app';
+import Team from '../database/models/teams.model';
+import ITeam from '../Interfaces/ITeam';
 
 import { Response } from 'superagent';
-import Team from '../database/models/teams.model';
 
 chai.use(chaiHttp);
 
-const { app } = new App();
-
 const { expect } = chai;
 
-describe('Testa a rota /teams', () => {
+describe('Testes do Teams', () => {
 
-  let response: Response;
+  let chaiHttpResponse: Response;
 
-  afterEach(function() { sinon.restore() });
+  afterEach(()=>{
+    sinon.restore();
+  })
 
-  describe('Teste GET na rota /teams', () => {
-    it('Lista todos os times', async () => {
+  it('Get retorna todas os times', async () => {
+    chaiHttpResponse = await chai
+       .request(app).get('/teams')
 
-      sinon.stub(Team, "findAll").resolves(teamsMock as unknown as Team[]);
-
-      const response = await chai
-              .request(app)
-              .get('/teams');
-
-
-      expect(response.status).to.be.equal(200);
-      expect(response.body).to.be.deep.equal(teamsMock);
-
-    });
-    it('Buscar um time especifico pelo Id', async () => {
-
-      sinon.stub(Team, "findByPk").resolves(teamsMock[0] as unknown as Team);
-
-      const response = await chai
-              .request(app)
-              .get('/teams/1');
-
-
-      expect(response.status).to.be.equal(200);
-      expect(response.body).to.be.deep.equal(teamsMock[0]);
-
-    });
+    expect(chaiHttpResponse.status).to.be.deep.equal(200);
   });
+
+  it('Testando get by id retorna um time', async () => {
+    chaiHttpResponse = await chai
+       .request(app).get('/teams/5');
+
+    expect(chaiHttpResponse.status).to.be.deep.equal(200);
+  });
+
 });
